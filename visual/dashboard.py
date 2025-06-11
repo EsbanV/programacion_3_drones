ef8cefebc2ec2af6689fbe80117c7b8fb3b9671c
sasi372 <cdarwitg2024@alu.uct.cl>
2025-06-10 22:27:40 -0400

    le falta aun a la seccion de graficos porseacaso

---
+++ visual/dashboard.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk
+from visual.statics import GeneralStatisticsTab
+
+class Dashboard(ttk.Notebook):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.stats_tab = GeneralStatisticsTab(self)
+        self.add(self.stats_tab, text="Estadísticas")
